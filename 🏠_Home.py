60ac556273ae4c41a5e17a4c36dde288fa007089
Shoeb Shaikh <90shoeb@gmail.com>
2023-06-28 10:39:29 +0800

    adding menu and sketch generator files

---
+++ 🏠_Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to our POC app! 👋")
+
+st.sidebar.success("Select a demo above.")
+
